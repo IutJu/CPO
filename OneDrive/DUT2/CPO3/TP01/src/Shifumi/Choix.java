94b9d1c78013901bd92bc029601243469b5bd9ce
mhoangvslev <minhhoangdang@hotmail.com>
2016-09-13 21:28:25 +0200

    Finalization du projet: teste', verifie'

--- OneDrive/DUT2/CPO3/TP01/src/Shifumi/Choix.java
+++ OneDrive/DUT2/CPO3/TP01/src/Shifumi/Choix.java
@@ -3,18 +3,21 @@ package Shifumi;
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("1f3a55a8-fbe1-4220-94e5-a33aa6a7745c")
+/**
+ * Enumeration des choix possibles (PIERRE, PAPIER, CISEAUX)
+ */
 public enum Choix {
-    PIERRE(0),
-    PAPIER(1),
-    CISEAUX(2);
+    PIERRE,
+    PAPIER,
+    CISEAUX;
     
-    private final int value;
+    /*private final int value;
     private Choix(int value){
         this.value = value;
     }
     
     public int getValue(){
         return value;
-    }
+    }*/
 }
 
